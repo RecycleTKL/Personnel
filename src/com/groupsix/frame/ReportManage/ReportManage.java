878947b12ee479624c19c9ff2060adcee4457437
大宝 <@LAPTOP-CUS823CA>
2019-12-20 01:28:13 +0800

    初步完善报表

--- src/com/groupsix/frame/ReportManage/ReportManage.java
+++ src/com/groupsix/frame/ReportManage/ReportManage.java
@@ -25,6 +25,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JLabel;
 
 public class ReportManage extends JInternalFrame {
 	private JTable table;
@@ -35,14 +38,14 @@ public class ReportManage extends JInternalFrame {
 	 */
 	public ReportManage() {
 		setBounds(100, 100, 950, 757);
-		setLayout(new BorderLayout(0, 0));
+		getContentPane().setLayout(new BorderLayout(0, 0));
 		setMaximizable(true);
 		setIconifiable(true);
 		setResizable(true);
 		setClosable(true);
 		
 		JPanel panel = new JPanel();
-		add(panel, BorderLayout.NORTH);
+		getContentPane().add(panel, BorderLayout.NORTH);
 		
 		JButton Button_baobiao = new JButton("\u751F\u6210\u62A5\u8868");
 		Button_baobiao.addActionListener(new ActionListener() {
@@ -63,6 +66,26 @@ public class ReportManage extends JInternalFrame {
 				
 			}
 		});
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"\u8BF7\u9009\u62E9\u65F6\u95F4\u8303\u56F4", "\u6309\u6708\u5EA6", "\u6309\u5B63\u5EA6", "\u6309\u5E74\u5EA6"}));
+		panel.add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"\u8BF7\u9009\u62E9\u5E74\u4EFD", "2019\u5E74", "2020\u5E74", "2021\u5E74", "2022\u5E74", "2023\u5E74", "2024\u5E74", "2025\u5E74"}));
+		panel.add(comboBox_1);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"\u8BF7\u9009\u62E9\u5B63\u5EA6", "\u7B2C\u4E00\u5B63\u5EA6", "\u7B2C\u4E8C\u5B63\u5EA6", "\u7B2C\u4E09\u5B63\u5EA6", "\u7B2C\u56DB\u5B63\u5EA6"}));
+		panel.add(comboBox_3);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"\u8BF7\u9009\u62E9\u6708\u4EFD", "1\u6708", "2\u6708", "3\u6708", "4\u6708", "5\u6708", "6\u6708", "7\u6708", "8\u6708", "9\u6708", "10\u6708", "11\u6708", "12\u6708"}));
+		panel.add(comboBox_2);
+		
+		JLabel lblNewLabel = new JLabel("           ");
+		lblNewLabel.setToolTipText("                               ");
+		panel.add(lblNewLabel);
 		Button_baobiao.setBounds(481, 444, 120, 40);
 		panel.add(Button_baobiao);
 		
@@ -73,8 +96,9 @@ public class ReportManage extends JInternalFrame {
 			}
 		});
 		panel.add(button);
+		scrollPane.setToolTipText(" ");
 		
-		add(scrollPane, BorderLayout.CENTER);
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
 		/**
 		 * 显示所有信息在表格
 		 * @throws ClassNotFoundException
@@ -103,7 +127,8 @@ public class ReportManage extends JInternalFrame {
 			columnName.add("医疗保险");
 			columnName.add("工伤保险");
 			columnName.add("住房公积金号");
-			columnName.add("");
+			columnName.add("考勤情况");
+			columnName.add("实发工资");
 
 			
 			String dbClassName = "com.mysql.cj.jdbc.Driver";
@@ -112,9 +137,9 @@ public class ReportManage extends JInternalFrame {
 			String dbPwd = "Mystudi0";
 			Class.forName(dbClassName);				
 		 	conn = DriverManager.getConnection(dbUrl,dbUser,dbPwd);
-		 	String sql = "select tb_duty_info.id,tb_record.`name`,tb_duty_info.dept_id,tb_duty_info.duty_id,tb_duty_info.accession_date,tb_duty_info.accession_form_id,tb_duty_info.bank_name,tb_duty_info.bank_NO,tb_duty_info.society_safety_NO,tb_duty_info.annuity_safety_NO,tb_duty_info.medicare_safety_NO,tb_duty_info.compo_safety_NO,tb_duty_info.accumulation_fund_NO,tb_timecard.account_item_id\r\n" + 
-		 			"FROM tb_record,tb_duty_info,tb_timecard\r\n" + 
-		 			"WHERE tb_record.id=tb_duty_info.id AND tb_duty_info.id=tb_timecard.id;";
+		 	String sql = "select tb_duty_info.id,tb_record.`name`,tb_duty_info.dept_id,tb_duty_info.duty_id,tb_duty_info.accession_date,tb_duty_info.accession_form_id,tb_duty_info.bank_name,tb_duty_info.bank_NO,tb_duty_info.society_safety_NO,tb_duty_info.annuity_safety_NO,tb_duty_info.medicare_safety_NO,tb_duty_info.compo_safety_NO,tb_duty_info.accumulation_fund_NO,tb_timecard.account_item_id,tb_reckoning_info.money\r\n" + 
+		 			"FROM tb_record,tb_duty_info,tb_timecard,tb_reckoning_info\r\n" + 
+		 			"WHERE tb_record.id=tb_duty_info.id AND tb_duty_info.id=tb_timecard.id AND tb_duty_info.id=tb_reckoning_info.id;";
 		 	stmt = conn.prepareStatement(sql);
 			conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 			rs = stmt.executeQuery();
@@ -133,6 +158,16 @@ public class ReportManage extends JInternalFrame {
 				V.add(rs.getString(11));
 				V.add(rs.getString(12));
 				V.add(rs.getString(13));
+				if(rs.getString(14).equals("迟到")||rs.getString(14).equals("早退")) {
+					V.add(rs.getString(14)+"扣50元");
+					V.add(Integer.parseInt( rs.getString(15))-50);
+				}else if(rs.getString(14).equals("加班")){
+					V.add(rs.getString(14)+"加1000元");
+					V.add(Integer.parseInt( rs.getString(15))+1000);
+				}else {
+					V.add(rs.getString(14));
+					V.add(rs.getString(15));
+				}
 				rowData.add(V);
 			}
 			//SetNull();
